664c8692e48c4f62fafdd311d390429bb516a8d2
Hugo Alberto Bravo Bonilla <hugo@Hugos-iMac-Pro.local>
2019-12-05 12:38:42 -0600

    Homework completed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,152 @@
 // You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
-// ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
-// ```
+var friendsArray = [
+    {
+        name: "Mickey Mouse",
+        photo: "https://cbsnews3.cbsistatic.com/hub/i/r/2010/12/03/79ed9c0e-a644-11e2-a3f0-029118418759/resize/620x465/94b66abc1317bbbd3581ad4f2c4bc7a7/365073.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Tweety Bird",
+        photo: "https://i.pinimg.com/originals/ce/5f/53/ce5f53437e291c48705428721406985c.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Popeye",
+        photo: "https://secure.i.telegraph.co.uk/multimedia/archive/01693/Popeye_1693176c.jpg",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Pinocchio",
+        photo: "https://s2.dmcdn.net/v/AKhm91L_mO6mVALEC/x1080",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Homer",
+        photo: "https://cdn.pastemagazine.com/www/blogs/lists/2010/05/13/homer_simpson_300.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Bart",
+        photo: "http://wackymania.com/image/2011/3/childrens-cartoon-character-on-drugs/childrens-cartoon-character-on-drugs-01.jpg",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Apu",
+        photo: "https://cdn-3.expansion.mx/dims4/default/8e18ac8/2147483647/strip/true/crop/936x562+0+0/resize/800x480!/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2F40%2F3c%2Fe8b2ffe0475faaebc4fdef72f5f7%2Fapu.JPG",
+        scores: [
+            "1",
+            "5",
+            "4",
+            "5",
+            "1",
+            "1",
+            "3",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Lisa",
+        photo: "http://images.yodibujo.es/_uploads/_tiny_galerie/20130414/lisa-simpson-hija_6zs.jpg",
+        scores: [
+            "1",
+            "5",
+            "3",
+            "1",
+            "1",
+            "4",
+            "2",
+            "4",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Shrek",
+        photo: "https://holatelcel.com/wp-content/uploads/2018/11/HEAD-Shrek-reboot.jpg",
+        scores: [
+            "1",
+            "5",
+            "4",
+            "5",
+            "1",
+            "3",
+            "3",
+            "2",
+            "4",
+            "1"
+        ]
+    }
+];
 
-// 6. Determine the user's most compatible friend using the following as a guide:
 
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example:
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
-//    * The closest match will be the user with the least amount of difference.
 
-// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//    * The modal should display both the name and picture of the closest match.
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,94 @@
 
 //    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+
+
+
+
+
+
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friends = require("../data/friends");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
+    app.post('/api/friends', function (req, res) {
+        //grabs the new friend's scores to compare with friends in friendList array
+        var newFriendScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+
+        //runs through all current friends in list
+        for (var i = 0; i < friends.length; i++) {
+            var scoresDiff = 0;
+            //run through scores to compare friends
+            for (var j = 0; j < newFriendScores.length; j++) {
+                scoresDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScores[j])));
+            }
+
+            //push results into scoresArray
+            scoresArray.push(scoresDiff);
+        }
+
+        //after all friends are compared, find best match
+        for (var i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestMatch]) {
+                bestMatch = i;
+            }
+        }
+
+        //return bestMatch data
+        var bff = friends[bestMatch];
+        res.json(bff);
+
+        //pushes new submission into the friendsList array
+        friends.push(req.body);
+
+
+
+
+
+
+
+        // ---------------------------------------------------------------------------
+        // I added this below code so you could clear out the table while working with the functionality.
+        // Don"t worry about it!
+
+        app.post("/api/clear", function (req, res) {
+            // Empty out the arrays of data
+            friends.length = 0;
+
+            res.json({ ok: true });
+        });
+    });
+};
